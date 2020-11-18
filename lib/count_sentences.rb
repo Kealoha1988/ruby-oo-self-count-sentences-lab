417db90ccb1abd0b3e635f2ac6aa48d7e41074a2
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-18 15:20:20 -0500

    commit tree fa6989d122927a9979bf86e20d2c5fadd00ba137

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.to_s.include?(".")
   end
 
   def question?
-
+    self.to_s.include?("?")
   end
 
   def exclamation?
-
+    self.to_s.include?("!")
   end
 
   def count_sentences
-
-  end
+    self.split(/[.?!]/).delete_if{|x| x.length < 2}.count
+end
 end
